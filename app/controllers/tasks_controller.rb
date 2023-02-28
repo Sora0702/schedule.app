500df7e6bdb357b81a9c943a6d7de1cf9bef642d
春日想楽 <kasugasora@kasugasougakunoMacBook-Air.local>
2023-02-28 14:38:30 +0900

    CRUD機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,20 +4,41 @@ class TasksController < ApplicationController
   end
 
   def new #予定の新規作成画面
+    @task = Task.new
   end
 
   def create #予定の新規作成処理
+    @task = Task.new(params.require(:task).permit(:title, :start_day, :finish_day, :all_day, :memo))
+    if @task.save
+      flash[:notice] = "新規のスケジュールを登録しました。"
+      redirect_to :tasks
+    else
+      render "new"
+    end
   end
 
   def show #予定の詳細表示画面
+    @task = Task.find(params[:id])
   end
 
   def edit #予定の編集画面
+    @task = Task.find(params[:id])
   end
 
   def update #予定の編集処理
+    @task = Task.find(params[:id])
+    if @task.update(params.require(:task).permit(:title, :start_day, :finish_day, :all_day, :memo))
+      flash[:notice] = "スケジュールメモを更新しました。"
+      redirect_to :tasks
+    else
+      render "edit"
+    end
   end
 
   def destroy #予定の削除処理
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:notice] = "スケジュールを削除しました。"
+    redirect_to :tasks
   end
 end
