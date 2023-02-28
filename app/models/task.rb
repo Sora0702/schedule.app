223abbd6f143ed751db679b88b3d4570df7714ad
春日想楽 <kasugasora@kasugasougakunoMacBook-Air.local>
2023-02-28 17:37:57 +0900

    バリデーションの追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,19 @@
 class Task < ApplicationRecord
+  #タイトル、開始日、終了日は必須項目
+  #タイトルは最大20文字
+  #終了日は、開始日以降の日付
+  #スケジュールメモは、最大５００文字
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :start_day, presence: true
+  validates :finish_day, presence: true
+  validates :memo, length: { maximum: 500 }
+  validate :finish_day_not_be_before_start_day
+
+  def finish_day_not_be_before_start_day
+    unless start_day.nil? == true || finish_day.nil? == true
+      if start_day > finish_day
+        errors.add(:finish_day, "開始日が終了日の後日となっています。")
+      end
+    end
+  end
 end
